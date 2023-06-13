9c28bfc8992d785b830e18a35e389748651b9f60
Maydel Rego <maidelrego@yahoo.com>
2023-06-12 20:57:37 -0500

    implemented analytics

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,18 +5,22 @@
  */
 
 // Plugins
-import { loadFonts } from './webfontloader'
-import vuetify from './vuetify'
-import pinia from '../store'
-import router from '../router'
+import { loadFonts } from "./webfontloader"
+import vuetify from "./vuetify"
+import pinia from "../store"
+import router from "../router"
+import { createGtm } from "@gtm-support/vue-gtm"
+import type { App } from "vue"
 
-// Types
-import type { App } from 'vue'
+const TagManagerId = import.meta.env.VITE_GTM_ID
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   loadFonts()
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+  app.use(vuetify).use(router).use(pinia)
+  app.use(
+    createGtm({
+      id: TagManagerId,
+      vueRouter: router
+    })
+  )
 }
